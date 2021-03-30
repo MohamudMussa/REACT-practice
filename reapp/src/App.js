4bae7f8936db400169fde823d862d08f560c537b
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-03-30 12:04:43 +0100

    Login Home

--- reapp/src/App.js
+++ reapp/src/App.js
@@ -1,27 +1,42 @@
 import './App.css';
 import Form from './components/Form';
-import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Switch, Route } from "react-router-dom";
 import Home from './components/Home';
-
+<Form />
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
         <Router>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/"> <Home /> </NavLink>
-              </li>
-            </ul>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to="/Home"> Home </NavLink>
+                </li>
+              </ul>
 
+              <ul>
+                <li>
+                  <NavLink to="/login"> Log in</NavLink>
+                </li>
+              </ul>
 
+            </nav>
+          </header>
+          <Switch>
 
+            <Route path="/Home">
+              <Home />
 
+            </Route>
 
-          </nav>
+            <Route path="/login">
+              <Form />
+            </Route>
+
+          </Switch>
 
 
 
